7299fb873778fec94d13787cffb6e2c689906b0a
Joel D'Autilio <joeldautilio@gmail.com>
2023-03-18 18:38:51 -0300

    202303181838

--- xmonad.hs
+++ xmonad.hs
@@ -1,20 +1,24 @@
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
 import XMonad
-
 import XMonad.Util.EZConfig
-import XMonad.Util.Ungrab
-import XMonad.Layout.Magnifier
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Util.ClickableWorkspaces (clickablePP)
 
-myLayout = avoidStruts $ tiled ||| Mirror tiled ||| noBorders Full
+myLayout =  smartSpacingWithEdge 10 $ avoidStruts $ tiled ||| Mirror tiled ||| noBorders Full
   where
     tiled   = Tall nmaster delta ratio
     nmaster = 1      -- Default number of windows in the master pane
     ratio   = 1/2    -- Default proportion of screen occupied by master pane
     delta   = 3/100  -- Percent of screen to increment by when resizing panes
+
+mySB = statusBarProp "xmobar" (clickablePP xmobarPP)
 
 myManageHook :: ManageHook
 myManageHook = composeAll
@@ -24,7 +28,12 @@ myLogHook :: X ()
 myLogHook = fadeInactiveLogHook 0.85
 
 main :: IO()
-main = xmonad $ ewmhFullscreen $ ewmh $ docks $ myConfig
+main = xmonad
+     . ewmhFullscreen
+     . ewmh
+     . docks
+     . withEasySB mySB defToggleStrutsKey
+     $ myConfig
 
 myConfig = def
   { terminal    = "kitty"
@@ -37,6 +46,11 @@ myConfig = def
   `additionalKeysP`
   [ ("M-p",   spawn "rofi -show combi")
   , ("M-S-f", spawn "rofi -show filebrowser")
+  , ("M-d",   spawn "rofi -show drun -run-shell-command '{terminal} -e zsh -ic \"{cmd} && read\"'")
+  , ("M-g",   do
+      toggleWindowSpacingEnabled
+      toggleScreenSpacingEnabled
+    )
   -- Media keys
   , ("<XF86AudioMute>",         spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle") 
   , ("<XF86AudioRaiseVolume>",  spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%")
